12c01d81e47db6ae9ddd239efe3126d5637767d9
wesley perez <wesley.m.perez@gmail.com>
2020-01-02 23:02:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
-        
+
         if !new_hash[name]
           new_hash[name] = {}
         end
@@ -19,5 +19,5 @@ def nyc_pigeon_organizer(data)
     end
   end
   new_hash
-  
+
 end
